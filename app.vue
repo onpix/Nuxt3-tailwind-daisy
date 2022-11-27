538b24090078334cb645e16a7dc34da2e6a16c60
onpix <32115448+onpix@users.noreply.github.com>
2022-11-27 15:46:14 +0800

    2 files modified

--- app.vue
+++ app.vue
@@ -4,6 +4,8 @@
     <div class="btn btn-primary m-4 hover:scale-125">hello daisy</div>
     <div class="btn btn-primary m-4 hover:scale-125">hello daisy</div>
     <div class="btn btn-primary m-4 hover:scale-125">hello daisy</div>
-    <div class="btn btn-primary m-4 hover:scale-125">hello daisy</div>
+    <div class="btn btn-primary some-custom-style m-4 hover:scale-125">
+      hello daisy
+    </div>
   </div>
 </template>
